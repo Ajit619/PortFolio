2fcbe59cfc8d278b330b5fa5ebf3f8e7730be97a
Spark Bank <sharmaajit258@gmail.com>
2022-11-16 16:57:58 +0530

    my first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ const NavContent = ({ setMenuOpen }) => (
         Contact
       </a>
     </div>
-    <a href="mailto:official.6packprogrammer@gmail.com">
+    <a href="mailto:sharmaajit258@gmail.com">
       <button>Email</button>
     </a>
   </>
--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlLJg0ZUjT9ugV_Avg0DYH_hZps3r7LwM",
-  authDomain: "portfolio1234-92290.firebaseapp.com",
-  projectId: "portfolio1234-92290",
-  storageBucket: "portfolio1234-92290.appspot.com",
-  messagingSenderId: "438212328017",
-  appId: "1:438212328017:web:e987393929202268c61670"
+  apiKey: "AIzaSyDYAW9F1kP29j38dfZuZxK4lppUkSLD25c",
+  authDomain: "react-portfolio-abhishek.firebaseapp.com",
+  projectId: "react-portfolio-abhishek",
+  storageBucket: "react-portfolio-abhishek.appspot.com",
+  messagingSenderId: "278164302078",
+  appId: "1:278164302078:web:797fe5960bb12b5fca1daf",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
-
 //////////////////////////////////
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
